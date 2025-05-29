a08f57526d68b8601b693792ec9791f0c222d129
DASSE <ngbokota_honore@live.com>
2025-05-29 04:29:09 +0100

    Concerption des premiers élements de l'interface de l'appliation

---
+++ R/dependances.R
@@ -0,0 +1,6 @@
+#charger les packages
+source("R/packages.R")
+
+#charger les données et fonctions
+source("R/data.R")
+source("R/fonctions.R")
---
+++ R/packages.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(shinyjs)
+library(shinyBS)
+library(bslib)#bootstrap
+
+#Jeux des données du monde
+library(rnaturalearthdata)
